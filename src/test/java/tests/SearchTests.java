d31f58a7597dafcd531682fd72980b3e2e3b8e80
cheshi-mantu <cheshi.mantu@gmail.com>
2020-06-07 20:41:50 +0300

    gitignore and main code

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -17,31 +16,19 @@ import static io.qameta.allure.Allure.step;
 import static helpers.Environment.*;
 
 @Epic("QA.GURU automation course")
-@Story("Search tests")
+@Story("Search tests with video")
 @Tag("search_tests")
 class SearchTests extends TestBase {
     @Test
     @Description("Open page, search for string, try to match")
     @DisplayName("Successful search test")
     void successfulSearch() {
-        step("Open search page by URL {urlstring}", ()-> open(urlstring));
-        step("Enter search test {searchstring}", ()->{
+        step("Open search page by URL " +urlstring , ()-> open(urlstring));
+        step("Enter search test " + searchstring, ()->{
             $(byName("q")).val(searchstring).pressEnter();
         });
         step("Check if searched string is present in search output", ()->{
             $("html").shouldHave(text(searchstring));
-        });
-    }
-    @Test
-    @Description("Open page, search for string, try to match, but match is wrong")
-    @DisplayName("Unsuccessful search test, this will always fail")
-    void unSuccessfulSearch() {
-        step("Open search page by URL {urlstring}", ()-> open(urlstring));
-        step("Enter search test {searchstring}", ()->{
-            $(byName("q")).val(searchstring).pressEnter();
-        });
-        step("Check if searched string is present in search output", ()->{
-            $("html").shouldHave(text("Привет из Уханя, сучечке"));
         });
     }
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.FileReadHelper;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,23 +13,30 @@ import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 import static helpers.Environment.selenide_remote;
 
 public class TestBase {
-
     @BeforeAll
     public static void setUp() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        System.out.println("Yeee-haaaaa. Sorry for dumb message, this is just a stub for now");
 //        Configuration.browser = "opera";
 //        Configuration.startMaximized = true;
     }
+
     @BeforeEach
-    void beforeEachTest(){
+    void beforeEachTest() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("EnableVNC", true);
         capabilities.setCapability("EnableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = selenide_remote;
+
+        if (selenide_remote == "null") {
+            Configuration.remote = FileReadHelper.getStringFromFile("selenide_remote.secret");
+        } else {
+            Configuration.remote = selenide_remote;
+        }
     }
+
     @AfterEach
-    public void closeBrowser(){
+    public void closeBrowser() {
         closeWebDriver();
     }
 }
